532bea3a5f521ee2f57f7f8f12a605f45116df00
David Sawyer <davidsawyer10@gmail.com>
2013-12-31 23:11:33 -0500

    drop some for loops in for extra cleanness

--- index.php
+++ index.php
@@ -6,31 +6,14 @@
         </div>
     </div>
     <div id="slides" class="slides">
-        <img src="/media/engagement/engagement01.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement02.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement03.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement04.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement05.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement06.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement07.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement08.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement09.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement10.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement11.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement12.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement13.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement14.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement15.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement16.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement17.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement18.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement19.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement20.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement21.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement22.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement23.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement24.jpg" height="600" width="900" alt="engagement">
-        <img src="/media/engagement/engagement25.jpg" height="600" width="900" alt="engagement">
+        <?php for ($i = 1; $i <= 25; $i++) {
+            if ($i < 10) {
+                $num = '0'.$i;
+            } else {
+                $num = $i;
+            }
+            echo '<img src="/media/engagement/engagement'.$num.'.jpg" height="600" width="900" alt="engagement">'."\n";
+        } ?>
     </div>
 </section>
 <?php include("footer.php"); ?>
--- our-story.php
+++ our-story.php
@@ -42,46 +42,14 @@
     </div>
     <div class="story">
         <div id="slides2" class="slides">
-            <img src="/media/together/together01.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together02.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together03.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together04.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together05.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together06.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together07.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together08.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together09.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together10.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together11.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together12.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together13.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together14.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together15.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together16.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together17.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together18.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together19.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together20.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together21.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together22.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together23.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together24.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together25.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together26.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together27.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together28.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together29.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together30.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together31.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together32.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together33.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together34.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together35.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together36.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together37.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together38.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together39.jpg" height="487" width="730" alt="together">
-            <img src="/media/together/together40.jpg" height="487" width="730" alt="together">
+            <?php for ($i = 1; $i <= 40; $i++) {
+                if ($i < 10) {
+                    $num = '0'.$i;
+                } else {
+                    $num = $i;
+                }
+                echo '<img src="/media/together/together'.$num.'.jpg" height="487" width="730" alt="together">'."\n";
+            } ?>
         </div>
         <div class="text">
             <p>During the fall of their sophomore year of high school, Alex and David began to catch each other's eye. David decided he better snatch Alex up while he had the chance, so he very sweetly knocked on her door and asked her to homecoming with a dozen roses (roses may not have been Alex's favorite flower, but David was definitely Alex's favorite boy). After a wonderful dance that they both wished could have lasted longer, they became better and better friends through lots of long walks, movies, and dates (it was very exciting when they turned 16 and could drive to these dates!). On January 17th, 2008, David asked Alex to be his girlfriend. Alex and David grew up together over the next six years. They were best friends who loved each other a whole lot. They both attended the University of Georgia, where David discovered a love for computers, and Alex decided she wanted to take care of sick people. They loved each other more and more everyday, and then one day...</p>
@@ -89,28 +57,14 @@
     </div>
     <div class="story">
         <div id="slides3" class="slides">
-            <img src="/media/proposal/proposal01.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal02.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal03.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal04.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal05.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal06.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal07.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal08.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal09.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal10.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal11.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal12.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal13.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal14.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal15.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal16.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal17.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal18.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal19.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal20.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal21.jpeg" height="487" width="730" alt="proposal">
-            <img src="/media/proposal/proposal22.jpeg" height="487" width="730" alt="proposal">
+            <?php for ($i = 1; $i <= 22; $i++) {
+                if ($i < 10) {
+                    $num = '0'.$i;
+                } else {
+                    $num = $i;
+                }
+                echo '<img src="/media/proposal/proposal'.$num.'.jpeg" height="487" width="730" alt="proposal">'."\n";
+            } ?>
         </div>
         <div class="text">
             <p>David came over to Alex's house to take her out on a date. First she made him watch Project Runway (did I mention he loves her?), and then they headed to Figo Pasta in Atlanta. After a delicious dinner, they were planning to go on a hike, but the rain was coming down in full force. David thought it would still be fun to go hiking and play in the rain so Alex agreed. As soon as they pulled into Sweetwater Creek State Park, the rain stopped, and the sun came out! They walked together through the woods, by the river, and through some cool ruins. Eventually, they reached the end of the path at a rock overlooking the river. After relaxing for a bit, Alex gave David one of his birthday gifts, a pair of Vans. As David tried on his new shoes, he pulled out a ring box. Alex was quite confused (Where did that come from?! Turns out he had unwrapped the gift, placed the ring in one of the shoes, and then rewrapped them the day prior). Then he held her hand, got down on one knee, said some very sweet things, and asked Alex to be his wife (YAY!). Of course, Alex thought that marrying David was the best idea EVER, so she said, "YES!" They ended the wonderful night with a big surprise party full of lots of special people and celebration. Now they can't wait to become husband and wife and to celebrate their marriage with all of their family and friends in June! Hope to see you there!</p>
