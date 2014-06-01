ff606965aa3de73f2c59f70a2fa0ab39991f15ff
David Sawyer <davidsawyer10@gmail.com>
2014-06-01 17:54:10 -0400

    fix countdown

--- footer.php
+++ footer.php
@@ -2,11 +2,20 @@
         <img class="logo" src="/media/AD.png" height="200" width="200" alt="A+D">
     </div>
     <footer>
-        <span class="countdown">See you in <?php
-            $date = strtotime("June 21, 2014 5:00 PM");
+        <span class="countdown"><?php
+            $date = strtotime("June 21, 2014 11:59 PM");
             $remaining = $date - time();
             $days = floor($remaining / 86400);
-            echo $days; ?> days!</span>
+            if ($days > 1) {
+                echo "See you in " . $days . " days!";
+            } else if ($days == 1) {
+                echo "See you tomorrow!";
+            } else if ($days == 0) {
+                echo "Today's the day!";
+            } else {
+                echo "Thanks for coming!";
+            }
+            ?></span>
         <span class="madeBy"><i class="tools"></i><span class="whom">Site built by David Sawyer. Branding by <a href="http://kimberlyoldenburg.com/">Kimmie Oldenburg</a>.</span></span>
     </footer>
     <script>
